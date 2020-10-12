d409141a60ef6d8e3cb5668dcb692856d18f9add
IngelParmas <58001671+IngelParmas@users.noreply.github.com>
2020-10-12 15:13:58 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -12,3 +12,16 @@ function newQuote() {
 	var randomNumber = Math.floor(Math.random() * (quotes.length));
 	document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
 }
+
+function move() {
+      let start = Date.now();
+
+      let timer = setInterval(function() {
+        let timePassed = Date.now() - start;
+
+        document.getElementById("britt").style.left = timePassed / 5 + 'px';
+
+        if (timePassed > 2000) clearInterval(timer);
+
+      }, 20);
+    }
